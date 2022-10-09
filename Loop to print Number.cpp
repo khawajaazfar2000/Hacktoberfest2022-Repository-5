ff79b6d7474b06c7d53b14b7dbc7481dfea5195b
Aleena Ali Azeem <99934126+aleenaazeem@users.noreply.github.com>
2022-10-10 01:30:46 +0500

    first commit

--- Loop to print Number.cpp
+++ Loop to print Number.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-	cout<<"************"<<endl;
-	cout<<"We are using FOR Loop to print its values."<<endl;
-	cout<<"************"<<endl;
+	//cout<<"************"<<endl;
+	cout<<"We are using FOR Loop to print its values. to get the value in a loop"<<endl;
 	
-	for(int i=0; i <=5; i++){
+	for(int i=0; i <=9; i++){
 	     cout<<i << endl;
+		 cout<<"1"<<endl;
 	}	
 	
 	return 0;
