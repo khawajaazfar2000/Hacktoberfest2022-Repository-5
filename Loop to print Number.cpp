9332873d28419497418e5d9ac76d9a7e41c83932
Muhammad Zohaib Arshad <muhammadzohaibarshad18@gmail.com>
2022-10-09 17:52:15 +0500

    do it

--- Loop to print Number.cpp
+++ Loop to print Number.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-	
+	cout<<"We are using FOR Loop to print its values."<<endl;
 	for(int i=0; i <=10; i++)
 	{
-		cout<<i <<"\n";
-	}
-	
+		cout<<i << endl;
+	}	
 	return 0;
 }
